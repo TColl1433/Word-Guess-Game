d8b24aeee40fafb4a29d0539b362828db977270e
Tracy Collins <tracyecollins15@gmail.com>
2019-03-16 09:02:20 -0400

    added background image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,59 @@
 
-        //var $ = function (id) {
-        //return document.getElementById();
-        //}
+//var $ = function (id) {
+//return document.getElementById();
+//}
 
-        // Create variables that hold references to the places in the HTML where we want to display things.
+// Create variables that hold references to the places in the HTML where we want to display things.
 
 
-        //declaring Arrays
-        var game = ["Madonna", "Prince", "Tina Turner", "Cindi Lauper"]
-                            //these variables give you random options from "var game"
-                            var choice = Math.floor(Math.random() * 4);
-                            var answer = game[choice];
-                            var myLength = answer.length;
-                            var display = [myLength];
-                            var win = myLength;
-                            var letters = answer.split("");
-                            var attemptsLeft = 10;
-                            var output = "";
-                    
-                    
-                            //console.log(answer);
-                    
-        var setup = function () {
-            for (var i = 0; i < answer.length; i++) {
-                                display[i] = "_ ";
-                            output = output + display[i];
-                        }
-                        document.getElementById("empty-div").innerHTML = output;
-                        output = "";
-                    }
-            
-        document.onkeyup = function () {
-            var userGuess = event.key;
-                            output = "";
-                
-            for (var j = 0; j < answer.length; j++) {
+//declaring Arrays
+var game = ["Madonna", "Prince", "Tina Turner", "Cindi Lauper"]
+//these variables give you random options from "var game"
+var choice = Math.floor(Math.random() * 4);
+var answer = game[choice];
+var myLength = answer.length;
+var display = [myLength];
+var win = myLength;
+var letters = answer.split("");
+var attemptsLeft = 10;
+var output = "";
 
-                if (userGuess == letters[j]) {
-                                display[j] = userGuess;
-                            win--;
-                        }
-        
-                        output = output + display[j] + " ";
-                    }
-                    document.getElementById("empty-div").innerHTML = output;
-                    output = " ";
-                    attemptsLeft--;
-            if (win < 1) {
-                                document.getElementById("guesses").innerHTML = "YOU WIN!";
-                            } else if (attemptsLeft < 1) {
-                                document.getElementById("guesses").innerHTML = "YOU LOSE!";
-                            } else {
-                                document.getElementById("guesses").innerHTML = "You have " + attemptsLeft + " guesses left"
-                            }
-                            }
-                            window.onload = setup();
-                    
+
+
+//this "iterates" through the variable array and displays an underscore for each letter          
+var setup = function () {
+    for (var i = 0; i < answer.length; i++) {
+        display[i] = "_ ";
+        output = output + display[i];
+    }
+    document.getElementById("empty-div").innerHTML = output;
+    output = "";
+}
+
+
+
+document.onkeyup = function () {
+    var userGuess = event.key;
+    output = "";
+
+    for (var j = 0; j < answer.length; j++) {
+
+        if (userGuess == letters[j]) {
+            display[j] = userGuess;
+            win--;
+        }
+
+        output = output + display[j] + " ";
+    }
+    document.getElementById("empty-div").innerHTML = output;
+    output = " ";
+    attemptsLeft--;
+    if (win < 1) {
+        document.getElementById("guesses").innerHTML = "YOU WIN!";
+    } else if (attemptsLeft < 1) {
+        document.getElementById("guesses").innerHTML = "YOU LOSE!";
+    } else {
+        document.getElementById("guesses").innerHTML = "You have " + attemptsLeft + " guesses left"
+    }
+}
+window.onload = setup();
